4042b15659cb56903aaeff56b8880a89d906460b
Ronaldo Adriano Ferreira <ronaldo@radriano.com>
2023-06-29 15:14:17 -0400

    Atualizações 2023-06-29_01

---
+++ games/crossFreeway/sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  const canvas =createCanvas(500, 400);
+  somDaTrilha.loop();
+}
+
+
+
+function draw() {
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialDoCarro();
+  verificaColisao();
+  incluiPontos();
+  marcaPonto();
+}
+
+
+
+
+
+
+
+
+
